103cfe2bdb53c5d5b7b70d152f571f6702fb9687
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-19 22:23:59 +0200

    User-friendly way to select configuration items

---
+++ assets/javascripts/cmdb.js
@@ -0,0 +1,28 @@
+jQuery(function($) {
+  window.selectifyConfigurationItemRelation = function() {
+    $("#relation_configuration_item_id").select2({
+      minimumInputLength: 1,
+      ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+        url: '../../configuration_items.json',
+        dataType: 'json',
+        data: function (term, page) {
+          return { search: term, limit: 10 }
+        },
+        results: function (data, page) {
+          result = { results: [] }
+          $.each(data.configuration_items, function() {
+            result.results.push({id: this.id, text: this.name})
+          })
+          return result
+        }
+      },
+      formatInputTooShort: function() { return "" },
+      formatSearching: function() { return "..." },
+      formatNoMatches: function() { return $("#relation_configuration_item_id").data("noMatches") },
+      formatResult: function(e) { return e.text },
+      zformatSelection: function() { },
+      zdropdownCssClass: "bigdrop"
+    });
+  }
+  selectifyConfigurationItemRelation()
+})
