c0c4dc9d0cc52a9d782a86be7ab4909233d56197
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-20 06:45:18 +0200

    Added <optgroup>s to isolate configuration items types

--- assets/javascripts/cmdb.js
+++ assets/javascripts/cmdb.js
@@ -9,10 +9,14 @@ jQuery(function($) {
           return { search: term, limit: 10, not: $("#relation_configuration_item_id").data("currentIds") }
         },
         results: function (data, page) {
-          result = { results: [] }
+          var groups = {}, result = { results: [] }
           $.each(data.configuration_items, function() {
-            result.results.push({id: this.id, text: this.name})
+            if (!groups[this.item_type]) { groups[this.item_type] = {text: this.item_type, children: []} }
+            groups[this.item_type].children.push({id: this.id, text: this.name})
           })
+          for (var group in groups) {
+            result.results.push(groups[group])
+          }
           return result
         }
       },
@@ -21,8 +25,8 @@ jQuery(function($) {
       formatNoMatches: function() { return $("#relation_configuration_item_id").data("noMatches") },
       formatResult: function(e) { return e.text },
       containerCssClass: 'configuration-items-select2',
-      zformatSelection: function() { },
-      zdropdownCssClass: "bigdrop"
+      dropdownCssClass: 'configuration-items-select2-dropdown',
+      zformatSelection: function() { }
     });
   }
   selectifyConfigurationItemRelation()
