9430381cc7ceb89e0b9520d2752a14a67d9c14e8
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-09 11:27:17 +0200

    Added original partial fingerprints to deface overrides

--- app/overrides/issues/show.rb
+++ app/overrides/issues/show.rb
@@ -1,4 +1,5 @@
 Deface::Override.new :virtual_path  => 'issues/show',
+                     :original      => '5c9c7359c91d5e72ad98984f169dcb0abf65173a',
                      :name          => 'add-configuration-items-to-issues',
                      :insert_after  => 'table.attributes',
                      :text          => '<%= render :partial => "configuration_items" %>'
